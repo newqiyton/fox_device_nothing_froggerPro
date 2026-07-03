395cd2372107a4a65b9d551f03b24b04f180c203
newqiyton <newqiyton@gmail.com>
2026-07-03 22:03:12 +0100

    Added  more touch drivers

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -3,7 +3,7 @@
 # Load touch drivers if they didn't load
 # Adapted from chickendrop89
 
-DRIVERS="panel_event_notifier qts goodix_ts st_fts touchpanel_event_notify"
+DRIVERS="panel_event_notifier qts goodix_ts st_fts touchpanel_event_notify ft3683g atmel_mxt_ts"
 MODULES_DIR="/lib/modules"
 
 for d in $DRIVERS; do
